1d313442512b71c279f1072d7dc163d5495e853c
yowanmada123 <byayoan@gmail.com>
2022-08-26 08:57:20 +0700

    add newpage

---
+++ msib1_remotehosting/lib/newpage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class NewPage extends StatefulWidget {
+  const NewPage({Key? key}) : super(key: key);
+
+  @override
+  State<NewPage> createState() => _NewPageState();
+}
+
+class _NewPageState extends State<NewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
